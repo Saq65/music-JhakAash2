4e537210a3cda73c12a67122caf2328613cb128e
saqlain shaikh <118292650+Saq65@users.noreply.github.com>
2023-04-24 00:08:53 +0530

    Update music.js

--- music.js
+++ music.js
@@ -78,7 +78,7 @@
         elm[u].style.background='#121212 ';
         elm[u].style.color='white'
         elm[u].style.boxShadow='2px 1px 1px white inset'
-       // elm[u].style.fontfamily='arial'
+        elm[u].style.fontfamily='arial'
        }
     }else{  
         const elm= document.querySelectorAll('li');
